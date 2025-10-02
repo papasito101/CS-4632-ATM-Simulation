15b080d4a0654c19ebaf7d2c83b2b825c0ca07b9
papasito101 <36241182+papasito101@users.noreply.github.com>
2025-10-02 17:53:22 -0400

    Update atm.py

--- src/atm.py
+++ src/atm.py
@@ -1,8 +1,10 @@
 class ATM(object):
     __slots__ = ("aid", "is_busy", "busy_time")
+    
     def __init__(self, aid):
         self.aid = aid
         self.is_busy = False
         self.busy_time = 0.0 
+        
     def __repr__(self):
         return "ATM(aid={}, busy={}, busy_time={:.4f}h)".format(self.aid, self.is_busy, self.busy_time)
