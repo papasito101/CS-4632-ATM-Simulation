b8b752db78f7d0a16d3ea2841d7d9c24e765281b
papasito101 <36241182+papasito101@users.noreply.github.com>
2025-10-02 17:53:07 -0400

    Update customer.py

--- src/customer.py
+++ src/customer.py
@@ -1,7 +1,9 @@
 class Customer(object):
     __slots__ = ("cid", "arrival_time")
+    
     def __init__(self, cid, arrival_time):
         self.cid = cid
         self.arrival_time = float(arrival_time)
+        
     def __repr__(self):
         return "Customer(cid={}, t={:.4f})".format(self.cid, self.arrival_time)
