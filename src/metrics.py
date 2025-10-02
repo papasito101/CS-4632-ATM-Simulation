fa6ed8dc8f4b117a67364ddf7ca0b7b9f020c6ef
papasito101 <36241182+papasito101@users.noreply.github.com>
2025-10-02 17:50:05 -0400

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,15 @@
 class Metrics(object):
     def __init__(self):
         self.arrivals = 0
-
+        self.balked = 0
+        self.completed = 0
+        self.started = 0
+        self.wait_times = []
     def inc_arrivals(self):
         self.arrivals += 1
+    def inc_balked(self): 
+        self.balked += 1
+    def inc_completed(self): 
+        self.completed += 1
+    def inc_started(self): 
+        self.started += 1
